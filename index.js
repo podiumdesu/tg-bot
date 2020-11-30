daaa8600b205cb7101958c49eab324c08858c695
podiumdesu <wwhmomo@gmail.com>
2020-11-30 20:21:36 +0800

    Add: new user

--- index.js
+++ index.js
@@ -41,21 +41,24 @@ bot.onText(/\/dylists/, (msg) => {
   bot.sendMessage(msg.chat.id, resMsg)
 })
 
-async function checkDyUpdates(msg_id, name, time = 5 * 10 * 1000) {
+async function checkDyUpdates(msg_id, name, time = 10 * 1000) {
   let previousData = (await check(dyJSON[name]))
   setInterval(async () => {
       let newData = (await check(dyJSON[name]))
       if (newData.focus !== previousData.focus) {
           bot.sendMessage(msg_id, `${name}目前关注数为${newData.focus}`)
       }
-      if (newData.works !== previousData.works) {
-          const resMsg = (newData > previousData) ? `${name}更新了${newData - previousData}部作品` : `${name}可能删减了${previousData - newData}部作品`
+      let change = newData.works - previousData.works
+      if (change !== 0) {
+        console.log(`${new Date()} check dy updates`)
+          const resMsg = (change > 0) ? `${name}更新了${change}部作品` : `${name}可能删减了${0-change}部作品`
           bot.sendMessage(msg_id, `${resMsg}, 点击${dyJSON[name].user}查看！`)
       }
       previousData = newData
   }, time)
 }
 checkDyUpdates(user.msg_id, "test")
+checkDyUpdates(user.msg_id, "胡楚靓", 5 * 60 * 1000)
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
 
